e98529c37e470b2a3b12fcd3c1eaee633834e74d
Sugandha Bansal <bansal.sugandha@gmail.com>
2017-12-26 16:21:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sugandhabansal/wordstack/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sugandhabansal.wordstack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void undo(View view) {
+    }
+
+    public void start(View view) {
+    }
+}
